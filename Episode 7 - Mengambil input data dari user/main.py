7a40d88234585e4bde097465d3bd0de5cb7fed3e
Makhasin Muhammad <167407468+mascynnn@users.noreply.github.com>
2024-06-29 13:19:30 +0700

    Create main.py

---
+++ Episode 7 - Mengambil input data dari user/main.py
@@ -0,0 +1,17 @@
+# input dari user
+
+# data yang dimasukkan pasti string
+data = input("input nama = ")
+
+print("nama anda adalah ", data, "type data : ", type(data))
+
+# jika ingin data yang ingin dimasukkan berupa integer
+data_int = int(input("input integer : "))
+
+print("nilai yang anda masukkan adalah: ", data_int, "dengan type data : ", type(data_int))
+
+# input data berupa boolean
+biner = bool(int(input("input boolean : ")))
+
+print("data : ", biner, "type : ", type(biner))
+
