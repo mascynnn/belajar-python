098ef61bedb991705829a4a494512b2946b68b1c
Makhasin Muhammad <167407468+mascynnn@users.noreply.github.com>
2024-06-29 13:15:37 +0700

    Create main.py

---
+++ Episode 4 - Mengenal variabel/main.py
@@ -0,0 +1,21 @@
+# variabel adalah tempat menyimpan data
+a = 10
+b = 5
+
+# pemanggilan pertama
+print("nilai a = ", a)
+print("nilai b = ", b)
+
+# penamaan variabel
+nilai_terbesar = b
+angka10 = 10
+nilaiA = 17.5
+
+# pemanggilan kedua
+print("nilai a = ", a)
+a = 7
+print("nilai a = ", a)
+
+# assignment indirect
+b = a
+print("nilai b = ", b)
